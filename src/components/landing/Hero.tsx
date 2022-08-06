e7b9f2901f910251c61907ac30a0f7896f2a49d5
glitch-afk <rahul004@protonmail.ch>
2022-08-06 18:57:15 +0530

    refactor: minor changes

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -5,8 +5,8 @@ import SwapButton from './SwapButton';
 const Hero = () => {
   return (
     <section className="lg:custom-bg dark:bg-none">
-      <div className="mx-auto min-h-[80vh]  max-w-8xl px-4 sm:px-6 lg:px-8">
-        <div className="mt-28 flex w-full flex-col justify-center text-center lg:mt-40">
+      <div className="mx-auto min-h-[80vh] max-w-8xl px-4 sm:px-6 lg:px-8">
+        <div className="mt-36 flex w-full flex-col justify-center text-center sm:mt-52">
           <h1 className="text-5xl font-extrabold text-fetcch-dark dark:text-white xl:text-7xl">
             Seamless <br />
             <span className="font-extrabold text-fetcch-mustard">
@@ -14,7 +14,7 @@ const Hero = () => {
             </span>{' '}
             Swapping
           </h1>
-          <h2 className="mt-4 text-lg font-semibold text-fetcch-dark dark:text-white lg:text-2xl">
+          <h2 className="mt-4 px-6 text-lg font-semibold text-fetcch-dark dark:text-white lg:text-2xl">
             At wagpay we power a seamless bridge for your swap
           </h2>
           <div className="mt-10 flex w-full justify-center space-x-4">
--- src/components/landing/Sayhello.tsx
+++ src/components/landing/Sayhello.tsx
@@ -2,7 +2,7 @@ import { WalletGroupIcon } from '@/icons/walletsGroup';
 
 const Sayhello = () => {
   return (
-    <div className="mx-auto flex min-h-screen max-w-8xl flex-col items-center justify-start px-4 sm:px-6 lg:px-8">
+    <div className="mx-auto flex min-h-screen max-w-8xl flex-col items-center justify-start px-4 pt-20 sm:px-6 lg:px-8">
       <h2 className="mb-10 text-center text-2xl font-bold lg:mb-20 lg:text-4xl">
         Say hello to hassle free bridge selction for <br />
         <span className="text-fetcch-mustard">crosschain</span>&nbsp;
