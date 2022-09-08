fe53b2e447923610e73ab14739d2d92dd677ac98
shivam2320 <alekarshivam23@gmail.com>
2022-09-08 20:43:56 +0530

    swap

--- src/hooks/useBridge.ts
+++ src/hooks/useBridge.ts
@@ -1,4 +1,5 @@
 import type { Coin } from '@/types';
+import { ethers, Signer } from 'ethers';
 
 interface Return {
   fees: number;
@@ -42,7 +43,41 @@ export const useBridge = () => {
     return fees;
   };
 
+  const swap = async() => {
+
+    // LZ BSC -> 0xD187329cEAe8F0f08d7fbb51444ac301B0908616
+    // LZ Polygon -> 0x6fB5dd7f24E6B85E7e8019C40b9E628B0BA86EE0
+
+    // Bridge BSC -> 0xEF16C64E352b97518958Ae9C3D81a5d53c602F0B
+    // Bridge Polygon -> 0x423e25c00eA0fDD65a9dC104dD7996842e66d94e
+
+    const contract = new ethers.Contract(//address, abi, signer);
+
+    const swapdata = [
+      //refund address,
+      [
+        //from token,
+        //to token (USDC/BUSD),
+        //amount,
+        //dex required (true/false)
+      ],
+      [
+        //from token (USDC/BUSD),
+        //to token,
+        //destination (layerzero implementation on toChain),
+        //dex required (true/false)
+      ],
+      //receiver,
+      //fromChain id
+      //toChain id
+    ]
+
+    const tx = await contract.swap(swapdata, {value: ethers.utils.parseEther(fromChain === BSC ? '0.01' : fromChain === POLY ? '2'), gasPrice: signer.provider?.getGasPrice(), gasLimit: 15000000});
+    await tx;
+  }
+
   return {
+    swap,
     estimateFees,
   };
 };
